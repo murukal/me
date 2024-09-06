9e4b8dfce4049710b6309ab8b5b5ec7bb6ca2d42
murukal <tutu@fantufantu.com>
2024-09-06 18:01:31 +0800

    feat: change home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
-import Image from 'next/image'
+import LearnCoding from '@/components/learn-coding'
 
 const Home = () => {
   return (
     <div>
-      <div>
-        <div>
-          <h3>Hi, I`m tutu</h3>
-          <h3>Front End Dev</h3>
+      <div className='px-10'>
+        <div className='max-w-screen-2xl mx-auto grid gap-10 grid-cols-2 justify-items-center items-center'>
+          <section className='w-fit'>
+            <h3>Hi, I`m tutu</h3>
+            <h3>Front End Dev</h3>
+          </section>
+          <div
+            // className='w-fit'
+            style={{
+              width: 'fit-content(470px)'
+            }}
+          >
+            <LearnCoding />
+          </div>
         </div>
-        <div></div>
       </div>
+
+      {/* categories */}
+      <div></div>
     </div>
   )
 }
