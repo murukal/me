65ab008b116ecf6ffcf7efb9c194e5821f610291
murukal <tutu@fantufantu.com>
2024-10-07 22:19:49 +0800

    feat: use remote deps avoid server render error

--- api/index.tsx
+++ api/index.tsx
@@ -1,6 +1,5 @@
 import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client'
 import { onError } from '@apollo/client/link/error'
-import { Notification } from 'musae'
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -10,9 +9,11 @@ const client = new ApolloClient({
       const errorMessage = graphQLErrors?.[0].message ?? networkError?.message
       if (!errorMessage) return
 
-      Notification.error({
-        title: '接口调用异常！',
-        description: errorMessage
+      import('musae').then((_) => {
+        _.Notification.error({
+          title: '接口调用异常！',
+          description: errorMessage
+        })
       })
     }),
     new HttpLink({
