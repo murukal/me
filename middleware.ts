172c4af1f7a74fcd1114dfda294e770b09e819f6
murukal <tutu@fantufantu.com>
2024-10-10 09:33:29 +0800

    feat: add login path & middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
-import type { NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 
 const middleware = (request: NextRequest) => {
   const _authenticated = request.nextUrl.searchParams.get('authenticated')
   if (_authenticated) {
+    const response = NextResponse.next()
+
     const url = new URL(request.url)
     url.searchParams.delete('authenticated')
     return Response.redirect(url)
