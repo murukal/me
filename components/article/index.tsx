bf9fa188d4b5b6b0da8c8d30eeac1bde9da5a09f
murukal <tutu@fantufantu.com>
2025-01-05 23:30:16 +0800

    feat: upgrade musae

--- components/article/index.tsx
+++ components/article/index.tsx
@@ -2,34 +2,17 @@
 
 import type { Article as ArticleType } from '@/api/article.type'
 import Box from '../box'
-import { Skeleton, useTheme, VisuallyHidden } from 'musae'
+import { useTheme, Markdown } from 'musae'
 import dayjs from 'dayjs'
 import ArticleFooter from './footer'
-import dynamic from 'next/dynamic'
-import { useDefault } from '@aiszlab/relax'
 
 interface Props {
   article: ArticleType
   html: string
 }
 
-const Article = ({ article, html }: Props) => {
+const Article = ({ article }: Props) => {
   const theme = useTheme()
-
-  const loading = useDefault(() => {
-    return (
-      <Skeleton className='h-[800px] rounded-lg'>
-        <VisuallyHidden dangerouslySetInnerHTML={{ __html: html }} />
-      </Skeleton>
-    )
-  })
-
-  const RichTextEditor = useDefault(() => {
-    return dynamic(() => import('musae').then(({ RichTextEditor }) => RichTextEditor), {
-      ssr: false,
-      loading: () => loading
-    })
-  })
 
   return (
     <Box className='text-base p-5'>
@@ -44,7 +27,7 @@ const Article = ({ article, html }: Props) => {
         {dayjs(article.createdAt).format('MMM DD')}
       </p>
 
-      <RichTextEditor defaultValue={article.content} use='markdown' disabled fallback={loading} />
+      <Markdown value={article.content} />
 
       <ArticleFooter />
     </Box>
