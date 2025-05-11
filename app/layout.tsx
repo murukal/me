2c5b691eb172ee59280a8984c1188cb29ab97eaa
murukal <tutu@fantufantu.com>
2025-05-11 16:27:48 +0800

    chore: update workspace and configuration files
    - Added pnpm workspace configuration to only include built dependencies for '@tailwindcss/oxide'.
    - Removed deprecated PostCSS configuration file (postcss.config.cjs).
    - Created new PostCSS configuration file (postcss.config.mjs) with updated plugin syntax for Tailwind CSS.
    - Updated TypeScript configuration (tsconfig.json) to set the target to ESNext.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,57 +1,51 @@
-import './globals.css'
+import "./globals.css";
 
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import Provided from '@/components/provided'
-import { cookies } from 'next/headers'
-import { stringify } from '@aiszlab/relax/class-name'
-import { ApplicationToken } from '@/assets/token'
-import { GoogleAnalytics } from '@next/third-parties/google'
-import { type ReactNode } from 'react'
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import Provided from "@/components/provided";
+import { cookies } from "next/headers";
+import { stringify } from "@aiszlab/relax/class-name";
+import { ApplicationToken } from "@/assets/token";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { type ReactNode } from "react";
 
 interface Props {
-  children: ReactNode
+  children: ReactNode;
 }
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900'
-})
-
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900'
-})
+const geist = Geist({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'tutu.me',
-  description: 'welecome to tutu.me'
-}
+  title: "tutu.me",
+  description: "welecome to tutu.me",
+};
 
-const Layout = ({ children }: Props) => {
-  const authenticated = cookies().get(ApplicationToken.Authenticated)?.value
+const Layout = async ({ children }: Props) => {
+  const authenticated = (await cookies()).get(
+    ApplicationToken.Authenticated
+  )?.value;
 
   return (
-    <html lang='zh-cn'>
+    <html lang="zh-cn">
       <head>
         {!!authenticated && (
           <script
             dangerouslySetInnerHTML={{
-              __html: `globalThis.window.sessionStorage.setItem('${ApplicationToken.Authenticated}', '${authenticated}')`
+              __html: `globalThis.window.sessionStorage.setItem('${ApplicationToken.Authenticated}', '${authenticated}')`,
             }}
           />
         )}
       </head>
 
-      <body className={stringify(geistSans.variable, geistMono.variable, 'antialiased')}>
+      <body className={stringify(geist.className)}>
         <Provided>{children}</Provided>
       </body>
 
-      <GoogleAnalytics gaId='G-PSXENQ5JKD' />
+      <GoogleAnalytics gaId="G-PSXENQ5JKD" />
     </html>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
